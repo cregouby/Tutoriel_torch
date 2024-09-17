d227f65990f3b2a6056b8e1f92954c01a7903e79
Christophe REGOUBY <christophe.regouby@airbus.com>
2024-09-17 14:22:49 +0200

    install file

---
+++ 00_installation.R
@@ -0,0 +1,8 @@
+install.packages("pak")
+pak::pak(c("torch", 
+           "torchvision", "cregouby/torchvision.fr", 
+           "luz", "cregouby/luz.fr",
+           "mlverse/tok", "cregouby/tok.fr",
+           "mlverse/hfhub", "cregouby/hfhub.fr"
+           )
+         )
