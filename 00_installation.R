4289538ef3a186a2a12c6ebdbc006d209a4bf6d6
cregouby <cregouby@users.noreply.github.com>
2025-05-15 18:28:03 +0200

    Update 00_installation.R

--- 00_installation.R
+++ 00_installation.R
@@ -1,3 +1,4 @@
+# Installation
 install.packages("pak")
 pak::pak(c("torch", 
            "mlverse/torchvision", "cregouby/torchvision.fr", 
@@ -11,5 +12,16 @@ pak::pak(c("torch",
 
 # Vérification
 library(torch)
+# doit être TRUE
 torch_is_installed()
+
+# Puis
+#  - soit 
 backends_cudnn_is_available()
+#  - soit 
+backends_mps_is_available()
+#  - soit 
+backends_openmp_is_available()
+# - soit 
+backends_mkl_is_available()
+# - soit ...
