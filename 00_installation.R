b17f7fb0574adfa485da96bbef77c87335c52915
cregouby <cregouby@users.noreply.github.com>
2025-05-15 18:14:05 +0200

    Fix minhub is not on cran

--- 00_installation.R
+++ 00_installation.R
@@ -3,9 +3,9 @@ pak::pak(c("torch",
            "mlverse/torchvision", "cregouby/torchvision.fr", 
            "luz", "cregouby/luz.fr",
            "mlverse/tok", "cregouby/tok.fr",
-           "mlverse/hfhub", "cregouby/hfhub.fr"
+           "mlverse/hfhub", "cregouby/hfhub.fr",
            "mlverse/tabnet", "cregouby/tabnet.fr",
-           "tidymodels", "vip", "visdat", "minhub"
+           "tidymodels", "vip", "visdat", "mlverse/minhub"
            )
          )
 
