340398d8440d2f1287870f27c433e4aadb8317e1
C. Regouby <christophe.regouby@free.fr>
2025-06-11 21:09:49 +0200

    add exercice on model fine tuning

---
+++ 05_exercise/05_questions.R
@@ -0,0 +1,11 @@
+## ResNext model fine tuning
+
+# What is the influence of the dataloader `batch_size` on your CPU ?
+
+# What is the influence of the dataloader `batch_size` on your memory ?
+
+# What do we try to optimize with batch size ?
+
+# What is your ETA with optimal batch_size for one epoch ?
+
+# What is the optimal accelerator for your setup ?
